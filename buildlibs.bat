6b9c089cd1e1eb5459cb28919619c44e67aa5dd3
snaulX <alexandergunger666@gmail.com>
2020-08-11 12:52:45 +0300

    Create program1 files and some more

---
+++ buildlibs.bat
@@ -0,0 +1,4 @@
+@echo off
+java -jar Tangara.jar standard.mino -type TokensLibrary
+java -jar Tangara.jar collections.mino -type TokensLibrary
+java -jar Tangara.jar math.mino -type TokensLibrary
--- debugcompile.bat
+++ debugcompile.bat
@@ -1,4 +1,4 @@
 @echo off
-java -jar Tangara.jar program.mino
-"C:\Users\User\Desktop\World of Legends\TokensBuilder\tokensbuilder-dotnet\TokensBuilder\bin\Debug\TokensBuilder.exe" program.tokens
-ildasm program.exe
+java -jar Tangara.jar %1.mino
+"C:\Users\User\Desktop\World of Legends\TokensBuilder\tokensbuilder-dotnet\TokensBuilder\bin\Debug\TokensBuilder.exe" %1.tokens
+ildasm %1.exe
